0bdfb20a95a2546161fb30263be7ff42aba82a2a
Nox <mon3tr@protonmail.com>
2023-10-14 22:40:27 +0800

    hw03 (unfinished)

---
+++ hw03/Untitled-1.py
@@ -0,0 +1,75 @@
+def num_eights(pos):
+    """Returns the number of times 8 appears as a digit of pos.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> num_eights(8782089)
+    3
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if pos < 8:
+        return 0
+    elif pos == 8:
+        return 1
+    elif pos % 10 == 8:
+        return num_eights(pos // 10) + 1
+    else:
+        return num_eights(pos // 10)
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    n = k
+    while num_eights(n) >= 1:
+        k += 1
+    return k
+    while n % 8 == 0:
+        k -= 1
+    return k
